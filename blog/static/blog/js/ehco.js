dda942c18dd6b01beead4687ddf36f0015c6f8ea
ehco1996 <zh19960202@gmail.com>
2017-04-30 22:57:14 +0800

    粗略实现了了markdown-toc效果

--- blog/static/blog/js/ehco.js
+++ blog/static/blog/js/ehco.js
@@ -1,13 +1,48 @@
-function playPause() { 
-    var music = document.getElementById('music2'); 
-    var music_btn = document.getElementById('music_btn2'); 
-    if (music.paused){ 
-        music.play(); 
-         
-    } 
-    else{ 
-        music.pause(); 
-         
-    } 
-} 
+function playPause() {
+    var music = document.getElementById('music2');
+    var music_btn = document.getElementById('music_btn2');
+    if (music.paused) {
+        music.play();
 
+    }
+    else {
+        music.pause();
+
+    }
+}
+
+/* Display table of contents */
+function show_toc(toc_selector, wrap_id, min_nr)
+{
+    var wrap = document.getElementById(wrap_id);
+
+    var hlist = document.querySelectorAll(toc_selector);
+
+    if (!wrap)
+        return;
+     
+    if (!hlist || hlist.length <= min_nr) {
+        wrap.style.display = 'none';
+        return;
+    }
+   
+    
+
+    var ul = document.createElement('ul'), li, link;
+    
+    for (i = 0; i < hlist.length; i++) {
+        hlist[i].id = 'i_' + i;
+
+        li = document.createElement('li');
+
+        link = document.createElement('a');
+        link.href = '#' + hlist[i].id;
+        link.className = 'toc_link';
+        link.innerHTML = hlist[i].innerHTML;
+        
+        li.appendChild(link);
+        ul.appendChild(li);
+    }
+
+    wrap.appendChild(ul);
+}
--- static/blog/js/ehco.js
+++ static/blog/js/ehco.js
@@ -1,12 +1,11 @@
-function playPause() { 
-    var music = document.getElementById('music2'); 
-    var music_btn = document.getElementById('music_btn2'); 
-    if (music.paused){ 
-        music.play(); 
-         
-    } 
-    else{ 
-        music.pause(); 
-         
-    } 
+function playPause() {
+    var music = document.getElementById('music2');
+    var music_btn = document.getElementById('music_btn2');
+    if (music.paused) {
+        music.play();
+    }
+    else {
+        music.pause();
+    }
 } 
+
