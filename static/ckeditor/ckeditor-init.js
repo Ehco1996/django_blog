e447054918f59710f862d74281b8e018f214f2f7
ehco1996 <zh19960202@gmail.com>
2017-07-08 21:44:16 +0800

    css 文件编辑

---
+++ static/ckeditor/ckeditor-init.js
@@ -0,0 +1,46 @@
+(function() {
+  var djangoJQuery;
+  if (typeof jQuery == 'undefined' && typeof django == 'undefined') {
+    console.error('ERROR django-ckeditor missing jQuery. Set CKEDITOR_JQUERY_URL or provide jQuery in the template.');
+  } else if (typeof django != 'undefined') {
+    djangoJQuery = django.jQuery;
+  }
+
+  var $ = jQuery || djangoJQuery;
+
+  var el = $('.django-ckeditor-widget');
+  if (el.length && !window.CKEDITOR_BASEPATH) {
+    window.CKEDITOR_BASEPATH = el[0].getAttribute('data-ckeditor-basepath');
+  }
+
+  $(function() {
+    initialiseCKEditor();
+    initialiseCKEditorInInlinedForms();
+
+    function initialiseCKEditorInInlinedForms() {
+      try {
+        $(document).on("click", ".add-row a, .grp-add-handler", function () {
+          initialiseCKEditor();
+          return true;
+        });
+      } catch (e) {
+        $(document).delegate(".add-row a, .grp-add-handler", "click",  function () {
+          initialiseCKEditor();
+          return true;
+        });
+      }
+    }
+
+    function initialiseCKEditor() {
+      $('textarea[data-type=ckeditortype]').each(function(){
+        if($(this).data('processed') == "0" && $(this).attr('id').indexOf('__prefix__') == -1){
+          $(this).data('processed',"1");
+          $($(this).data('external-plugin-resources')).each(function(){
+              CKEDITOR.plugins.addExternal(this[0], this[1], this[2]);
+          });
+          CKEDITOR.replace($(this).attr('id'), $(this).data('config'));
+        }
+      });
+    }
+  });
+}());
