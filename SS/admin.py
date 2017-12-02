75603ede1b89761ecaa634ab308b0ae44144bb3a
ehco1996 <zh19960202@gmail.com>
2017-12-02 13:38:29 +0800

    修复后台配置文件bug

--- SS/admin.py
+++ SS/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
 
-from SS.models import MoneyRecord, ImageUrl, Foods
+from SS.models import ImageUrl, Foods
 
-
-class MoneyAdmin(admin.ModelAdmin):
-    list_display = ['info_code', 'amount', 'money_code', 'time', ]
-    search_fields = ['info_code', ]
-
-
-# Register your models here.
-admin.site.register(MoneyRecord, MoneyAdmin)
 admin.site.register(ImageUrl)
 admin.site.register(Foods)
